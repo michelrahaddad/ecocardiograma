bc28a2a3966c7f9d43229d17e1887d7147323275
michelrahaddad <104462721+michelrahaddad@users.noreply.github.com>
2025-06-27 10:54:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import os
 from app import app
-import routes
 
-# Configurações para produção
 if __name__ == "__main__":
-    # Configuração para desenvolvimento local
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
-else:
-    # Configuração para deploy (Render, Heroku, etc.)
-    # O Gunicorn irá usar esta instância
-    application = app
+    app.run(host="0.0.0.0", port=5000, debug=False)
