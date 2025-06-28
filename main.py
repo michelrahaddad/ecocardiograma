6533d01795975232d7712cd572621150a751ede8
michelrahaddad <104462721+michelrahaddad@users.noreply.github.com>
2025-06-28 08:35:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+
+```python
 """
 Sistema de Ecocardiograma - Grupo Vidah
 Entrada principal para deploy no Render - VERSÃO FINAL CORRIGIDA
 Este arquivo deve substituir o main.py no GitHub
 """
 
-from app_github_final import app
+from app import app
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=5000)
+```
